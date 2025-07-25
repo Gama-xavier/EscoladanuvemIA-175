a674e645fdf803a1d0955bcdc9f841182a23e884
Rafael <Rafaeldagamaxavier@gmail.com>
2025-07-24 21:35:38 -0300

    praticaIA

---
+++ pratica02/conversor_de_moedas..py
@@ -0,0 +1,9 @@
+valor_reais = 100.00
+taxa_dolar = 5.60
+taxa_euro = 6.60
+valor_dolares = valor_reais / taxa_dolar
+valor_euros = valor_reais / taxa_euro
+print(f"Valor em reais: R$ {valor_reais:.2f}")
+print(f"Valor em dólares: USD $ {valor_dolares:.2f}")
+print(f"Valor em euros: EUR € {valor_euros:.2f}")
+
